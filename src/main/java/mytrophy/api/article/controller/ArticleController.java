30393d12e83d59beda3323075d2fba604586fd99
soonduck <soonduck3729@gmail.com>
2024-07-01 17:52:09 +0900

    Refactor: 컨트롤러 단의 서비스 로직 모두 서비스로 이동

--- src/main/java/mytrophy/api/article/controller/ArticleController.java
+++ src/main/java/mytrophy/api/article/controller/ArticleController.java
@@ -12,6 +12,10 @@ import mytrophy.api.image.service.ImageService;
 import mytrophy.api.member.entity.Member;
 import mytrophy.api.member.service.MemberService;
 import mytrophy.api.querydsl.service.ArticleQueryService;
+import mytrophy.global.handler.InvalidHeaderException;
+import mytrophy.global.handler.InvalidRequestException;
+import mytrophy.global.handler.UnauthorizedException;
+import mytrophy.global.handler.resourcenotfound.ResourceNotFoundException;
 import mytrophy.global.jwt.CustomUserDetails;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -47,19 +51,14 @@ public class ArticleController {
     public ResponseEntity<ArticleResponseDto> createArticle(@AuthenticationPrincipal CustomUserDetails userInfo,
                                                             @Parameter(description = "게시글 제목, 내용, 게임 appId를 입력한다.") @RequestBody ArticleRequestDto articleRequestDto,
                                                             @Parameter(description = "firebase로 업로드된 파일 경로를 입력한다.") @RequestParam(value = "imagePath", required = false) List<String> imagePath) throws IOException {
-        //토큰에서 username 빼내기
+        // 토큰에서 username 빼내기
         String username = userInfo.getUsername();
         Member member = memberService.findMemberByUsername(username);
-
-        if (articleRequestDto.getImagePath() != null) {
-            imagePath = Arrays.asList(articleRequestDto.getImagePath());
-        } else {
-            imagePath = new ArrayList<>();
-        }
 
         // Article 생성
         ArticleResponseDto articleResponseDto = articleService.createArticle(member.getId(), articleRequestDto, imagePath);
 
+        // Http Status 응답
         return ResponseEntity.status(HttpStatus.CREATED).body(articleResponseDto);
     }
 
@@ -70,23 +69,7 @@ public class ArticleController {
     public ResponseEntity<Page<ArticleResponseDto>> getAllArticles(@PageableDefault(size = 10) Pageable pageable,
                                                                    @Parameter(description = "param으로 memberId를 입력하면 해당 Id가 생성한 게시글 리스트가 조회된다.") @RequestParam(required = false) Long memberId,
                                                                    @RequestParam(required = false, defaultValue = "false") boolean cntUp) {
-        Sort sort;
-        if (cntUp) {
-            sort = Sort.by("cntUp").descending();
-        } else {
-            sort = Sort.by("createdAt").descending();
-        }
-
-        pageable = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), sort);
-
-        Page<ArticleResponseDto> articles;
-
-        if (memberId != null) {
-            articles = articleService.findByMemberId(memberId, pageable);
-        } else {
-            articles = articleService.findAll(pageable);
-        }
-
+        Page<ArticleResponseDto> articles = articleService.getArticles(pageable, memberId, cntUp);
         return ResponseEntity.ok().body(articles);
     }
 
@@ -111,58 +94,33 @@ public class ArticleController {
     @Operation(summary = "말머리 별 게시글 리스트 조회", description = "말머리를 입력받아 해당 말머리의 게시글 리스트를 조회한다.")
     public ResponseEntity<Page<ArticleResponseDto>> getArticlesByHeader(@PathVariable Header header,
                                                                         @PageableDefault(size = 10) Pageable pageable) {
-        Page<ArticleResponseDto> articles;
-        // 헤더가 유효한지 검사
-        switch (header) {
-            case FREE_BOARD:
-            case INFORMATION:
-            case GUIDE:
-            case REVIEW:
-            case CHATING:
-                // 유효한 헤더인 경우 해당 헤더로 게시글 조회
-                Sort sort = Sort.by("createdAt").descending();
-                pageable = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), sort);
-                articles = articleService.findAllByHeader(header, pageable);
-                return ResponseEntity.ok().body(articles);
-            default:
-                // 잘못된 헤더인 경우 예외 처리
-                return ResponseEntity.badRequest().build();
+        try {
+            Page<ArticleResponseDto> articles = articleService.findAllByHeader(header, pageable);
+            return ResponseEntity.ok().body(articles);
+        } catch (InvalidHeaderException e) {
+            return ResponseEntity.badRequest().build();
         }
     }
 
     // 게시글 수정
     @PatchMapping("/{id}")
     @Operation(summary = "게시글 수정", description = "게시글 id를 입력받아 해당 게시글을 수정한다.")
-    public ResponseEntity updateArticle(@AuthenticationPrincipal CustomUserDetails userInfo,
-                                        @Parameter(description = "수정하려는 게시글 id를 입력한다.") @PathVariable("id") Long id,
-                                        @Parameter(description = "게시글의 내용을 입력한다.") @RequestBody ArticleRequestDto articleRequestDto,
-                                        @Parameter(description = "firebase에 업로드 된 이미지 경로를 입력한다.") @RequestParam(value = "imagePath", required = false) List<String> imagePath) throws IOException {
-        //토큰에서 username 빼내기
+    public ResponseEntity<?> updateArticle(@AuthenticationPrincipal CustomUserDetails userInfo,
+                                           @Parameter(description = "수정하려는 게시글 id를 입력한다.") @PathVariable("id") Long id,
+                                           @Parameter(description = "게시글의 내용을 입력한다.") @RequestBody ArticleRequestDto articleRequestDto,
+                                           @Parameter(description = "firebase에 업로드 된 이미지 경로를 입력한다.") @RequestParam(value = "imagePath", required = false) List<String> imagePath) throws IOException {
         String username = userInfo.getUsername();
         Long memberId = memberService.findMemberByUsername(username).getId();
-
-        // 유저 권한 확인
-        if (!articleService.isAuthorized(id, memberId)) {
+        try {
+            articleService.updateArticle(memberId, id, articleRequestDto, imagePath);
+            return ResponseEntity.status(HttpStatus.OK).build();
+        } catch (UnauthorizedException e) {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } catch (InvalidRequestException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
-
-        if (articleRequestDto.getHeader() == null || articleRequestDto.getName() == null || articleRequestDto.getContent() == null) {
-            return ResponseEntity.badRequest().build();
-        }
-
-        // 이미지 업로드 및 경로 설정
-        if (imagePath != null) {
-            articleRequestDto.setImagePath(String.join(",", imagePath));
-        }
-
-        // 파일을 변경하지 않았을 경우
-        if (articleRequestDto.getImagePath() == null) {
-            ArticleResponseDto article = articleService.findById(id);
-            articleRequestDto.setImagePath(article.getImagePath());
-        }
-
-        articleService.updateArticle(memberId, id, articleRequestDto);
-        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     // 게시글 삭제
@@ -181,37 +139,20 @@ public class ArticleController {
         return ResponseEntity.status(HttpStatus.CREATED).body(uploadFiles);
     }
 
-    // 파일만 삭제
-//    @DeleteMapping("/files")
-//    @Operation(summary = "파일 삭제", description = "firebase에 업로드된 파일을 삭제한다.")
-//    public ResponseEntity removeOnlyFiles(List<String> files) {
-//        imageService.removeFile(files);
-//        return ResponseEntity.ok().build();
-//    }
-
     // 게시글 추천
     @PostMapping("/{id}/like")
     @Operation(summary = "게시글 추천", description = "게시글 id를 입력받아 해당 게시글을 추천한다.")
     public ResponseEntity<String> likeArticle(@Parameter(description = "추천하려는 게시글 id를 입력한다.") @PathVariable("id") Long articleId,
                                               @AuthenticationPrincipal CustomUserDetails userInfo) {
+        String username = userInfo.getUsername();
+        Long memberId = memberService.findMemberByUsername(username).getId();
         try {
-            // 토큰에서 username 빼내기
-            String username = userInfo.getUsername();
-            Long memberId = memberService.findMemberByUsername(username).getId();
-
-            // 이미 해당 게시글에 좋아요를 눌렀는지 확인
-            boolean isLiked = articleService.checkLikeArticle(articleId, memberId);
-            if (isLiked) {
-                // 이미 좋아요를 눌렀다면 좋아요 취소
-                articleService.articleLikeDown(articleId, memberId);
-                return ResponseEntity.ok().body("게시글 추천을 취소하였습니다.");
-            } else {
-                // 좋아요를 누르지 않았다면 좋아요
-                articleService.articleLikeUp(articleId, memberId);
-                return ResponseEntity.ok().body("게시글을 추천하였습니다.");
-            }
+            String responseMessage = articleService.toggleArticleLike(articleId, memberId);
+            return ResponseEntity.ok().body(responseMessage);
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body("추천 실패");
+            return ResponseEntity.badRequest(). body("추천 실패");
         }
     }
 
